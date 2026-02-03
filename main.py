2f561f36585289b8314c9b4d1987299825fe3646
inikonoff <inikonoff@gmail.com>
2026-02-03 21:26:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 import asyncio
 import logging
+from aiohttp import web
 from aiogram import Bot, Dispatcher
 from config import config
 from src.handlers import start, testing, sos, tracker
 from src.database.supabase_client import db
 
+async def keep_alive():
+    app = web.Application()
+    app.router.add_get('/', lambda r: web.Response(text="I am alive"))
+    runner = web.AppRunner(app)
+    await runner.setup()
+    site = web.TCPSite(runner, '0.0.0.0', int(os.environ.get("PORT", 8080)))
+    await site.start()
+    
 async def main():
     logging.basicConfig(level=logging.INFO)
+    await keep_alive()
+    
+    print("Bot started...")
+    await dp.start_polling(bot)
     
     bot = Bot(token=config.BOT_TOKEN)
     dp = Dispatcher()
