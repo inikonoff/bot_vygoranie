0caa27e9fe4f84883b83871ccd0278736c82ad45
inikonoff <inikonoff@gmail.com>
2026-02-03 20:25:22 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,12 @@
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+class Config:
+    BOT_TOKEN = os.getenv("BOT_TOKEN")
+    SUPABASE_URL = os.getenv("SUPABASE_URL")
+    SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+    GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
+config = Config()
