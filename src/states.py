2de024346f485f1094e58f8d8ce104322b70ee80
inikonoff <inikonoff@gmail.com>
2026-03-08 21:46:07 +0300

    Add files via upload

--- src/states.py
+++ src/states.py
@@ -2,25 +2,26 @@ from aiogram.fsm.state import State, StatesGroup
 
 
 class TestStates(StatesGroup):
-    """Состояния для всех тестов"""
     mbi_q = State()
     boyko_q = State()
     phq9_q = State()
     gad7_q = State()
     pss10_q = State()
-    combined_phq_gad = State()
-
-
-class SOSStates(StatesGroup):
-    """Состояния для SOS-раздела"""
-    ai_chat = State()
-    anger_venting = State()
-    defusion = State()
-    stop_technique = State()
 
 
 class TrackerStates(StatesGroup):
-    """Состояния для дневника"""
     energy = State()
     emotion = State()
     gratitude = State()
+
+
+class AIState(StatesGroup):
+    waiting_for_query = State()
+
+
+class AngerState(StatesGroup):
+    venting = State()
+
+
+class DefusionState(StatesGroup):
+    waiting_for_thought = State()
